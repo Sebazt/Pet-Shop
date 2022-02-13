09dfc345810a8c82da69e650e55b98fd81f182cb
Sebazt <juan.londono29@udea.edu.co>
2022-02-13 02:33:11 -0500

    Actualización del js

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -1,38 +1,72 @@
+const search = document.querySelector(".input")
+search.addEventListener('keyup', searchInput)
+
 var productosFarmacia = [];
 var articulos = [];
+var valor = ""
+var filtrado = [];
 
 async function getDatos(){
   await fetch("https://apipetshop.herokuapp.com/api/articulos")
   .then((response) => response.json())
   .then((datos =>{
     articulos.push(...datos.response);
-    productosFarmacia = articulos.filter( farmacia => farmacia.tipo == "Medicamento")
-    console.log(articulos)
-    console.log(productosFarmacia)
-    mostrarFarmacia(productosFarmacia)
+    productosFarmacia.push(...articulos.filter( farmacia => farmacia.tipo == "Medicamento"))
+    mostrarFarmacia()
+    
+   
   }))
 }
 getDatos()
 
-function mostrarFarmacia(productosFarmacia){
+function searchInput(e){
+   let val = event.target.value
+   filtrado = productosFarmacia.filter(medic => medic.nombre.toLowerCase().includes(val.toLowerCase()))
+   console.log(filtrado)
+   mostrarFarmacia()
+}
+function mostrarFarmacia(){
   let product = ""
   let arregloFarmacia = []
-  arregloFarmacia.push(...productosFarmacia)
-  console.log(arregloFarmacia);
+
+  if(filtrado && filtrado.length > 0){
+    arregloFarmacia = []
+    arregloFarmacia.push(...filtrado)
+  console.log(filtrado)
+  }else{
+    arregloFarmacia = []
+    arregloFarmacia.push(...productosFarmacia)
+  }
   
   arregloFarmacia.map((producto) =>{
-        product +=
+    if(producto.stock < 5){
+       product +=
 
     `
         <div class="card">
           <img src="${producto.imagen}" alt="">
           <h3>${producto.nombre}</h3>
-          <span class="ultimos-items"></span>
+          <span class="ultimos-items">Ultimos Productos</span>
     
           <p class="precio">${producto.precio + "$"}</p>
           <b><a href="#" class="boton-carro">Añadir al carrito</a></b>
         </div> 
         `
+    }else {
+      product +=
+
+      `
+          <div class="card">
+            <img src="${producto.imagen}" alt="">
+            <h3>${producto.nombre}</h3>
+            <span class="ultimos-items"></span>
+      
+            <p class="precio">${producto.precio + "$"}</p>
+            <b><a href="#" class="boton-carro">Añadir al carrito</a></b>
+          </div> 
+          `
+    }
+       
   })
 
   document.querySelector(".contenedor").innerHTML = product
--- js/toy.js
+++ js/toy.js
@@ -1,29 +1,61 @@
-
+const search = document.querySelector(".boton")
+const items = document.querySelector(".ultimos-items")
+search.addEventListener("keyup", inputSearch)
 var juguetes = [];
 var articulos = [];
-
+let filtrado = [];
+let alerta = []
 async function getDatos(){
     await fetch("https://apipetshop.herokuapp.com/api/articulos")
     .then((response)=> response.json())
     .then((dato) =>{
         articulos.push(...dato.response)
-        juguetes = articulos.filter( juguete => juguete.tipo === "Juguete")
-        
-        mostrarProducto(juguetes)
+        juguetes.push(...articulos.filter( juguete => juguete.tipo === "Juguete"))
+        mostrarProducto()
+        showAlert()
     })
 }   
 getDatos()
 
-function mostrarProducto(juguetes){
+function inputSearch(e){
+    let valor = event.target.value
+    filtrado = juguetes.filter(toy=> toy.nombre.toLowerCase().includes(valor.toLowerCase()))
+    mostrarProducto()
+}
+function showAlert(){
+  let alertas = []
+  let newObjeto = {};
+  alertas.push(...juguetes)
+    
+}
+function mostrarProducto(){
     let product = ""
     let arregloJuguetes = []
-    arregloJuguetes.push(...juguetes)
-    console.log(arregloJuguetes)
 
+    if(filtrado && filtrado.length > 0){
+        arregloJuguetes = []
+        arregloJuguetes.push(...filtrado)
+    }else {
+        arregloJuguetes = []
+        arregloJuguetes.push(...juguetes)
+    }
+  
     arregloJuguetes.map((producto) => {
-        product +=
-        `
-        <div class="card">
+        if(producto.stock < 5) {
+           product += `
+           <div class="card">
+          <img src="${producto.imagen}" alt="">
+          <h3>${producto.nombre}</h3>
+          <span class="ultimos-items">Ultimas Unidades</span>
+    
+          <p class="precio">${producto.precio + "$"}</p>
+          <b><a href="#" class="boton-carro">Añadir al carrito</a></b>
+        </div> 
+           
+           `
+        }else{
+            product += `
+            <div class="card">
           <img src="${producto.imagen}" alt="">
           <h3>${producto.nombre}</h3>
           <span class="ultimos-items"></span>
@@ -31,8 +63,11 @@ function mostrarProducto(juguetes){
           <p class="precio">${producto.precio + "$"}</p>
           <b><a href="#" class="boton-carro">Añadir al carrito</a></b>
         </div> 
-        `
+            
+            `
+        }
     })
-    document.querySelector(".contenedor").innerHTML = product
-}
 
+document.querySelector(".contenedor").innerHTML = product
+
+}
